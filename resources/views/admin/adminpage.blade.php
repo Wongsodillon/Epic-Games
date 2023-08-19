a96e79c02e741b6f2be00c9ece68ccc0f6314b99
Wongsodillon <Wongsodillon@gmail.com>
2023-08-19 11:40:42 +0700

    Epic Games

--- resources/views/admin/adminpage.blade.php
+++ resources/views/admin/adminpage.blade.php
@@ -7,6 +7,7 @@
   <title>Admin</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <link rel="stylesheet" href="{{asset("css/adminpage.css")}}">
+  <link rel="icon" href="{{asset("images/epic-logo-white.png")}}">
 </head>
 <body>
   <div class="admin-header">
